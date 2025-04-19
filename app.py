40274401d7d0702f95a4127ec9e727ac98b18dae
Kalvin <kalvinsupriadi3@gmail.com>
2025-04-19 13:38:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ st.set_page_config(page_title="Synth", layout="wide")
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.image("images/logo_synth.png", width=100)
-    st.markdown(<h1>"Sȳnth"<br> "Get Ready With Sȳnth" </h1>)
+    st.markdown("<h1>Sȳnth<br>Get Ready With Sȳnth</h1>", unsafe_allow_html=True)
 
 # Upload PDF File
 uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
